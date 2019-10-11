1229254e097d056a6151f6a3034285b9ae543663
julian <juliohmhaha@hotmail.com>
2019-10-11 02:50:02 -0500

    astro

--- src/main/java/Facade/Facede.java
+++ src/main/java/Facade/Facede.java
@@ -10,13 +10,17 @@ import Adapter.Adaptador;
 import Adapter.Medico;
 import Adapter.Paciente;
 import Adapter.Usuario;
+import Composite.Componente;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Facede {
 
     private static Facede singleton;
     private HashMap<String, Usuario> usuarios;
-    private HashMap<String, EPS> eps1;
+    private EPS eps1;
+    private ArrayList<String> keyMedicos;
     
     
     public static Facede rConstructora() {
@@ -29,7 +33,8 @@ public class Facede {
 
     public Facede() {
         this.usuarios = new HashMap<>();
-        this.eps1 = new HashMap<>();
+        this.eps1 = new EPS();
+        this.keyMedicos = new ArrayList<>();
         this.addMedico("nikolas", "123");
         this.addPaciente("minichip", "123");
         this.addAdministrador("ballesgod", "123");
@@ -37,6 +42,7 @@ public class Facede {
 
     public void addMedico(String login, String password) {
         usuarios.put(login, new Medico(login, password));
+        this.keyMedicos.add(login);
     }
 
     public void addPaciente(String login, String password) {
@@ -47,10 +53,7 @@ public class Facede {
         usuarios.put(login, new Adaptador(login, password));
     }
 
-    public void addEPS(String nombre) {
-        eps1.put(nombre, new EPS(nombre));
-    }
-
+    
     public Usuario login(String login, String password) {
         Usuario log = usuarios.get(login);
         if (log != null && log.Acceder(login, password)) {
@@ -58,28 +61,55 @@ public class Facede {
         }
         return null;
     }
-    public String verContratos(String name){
-        EPS eps = eps1.get(name);
-        String m=null;
-        if(eps!=null){
-            m=eps.mostrarDatos();
+    public ArrayList<Componente> verContratos(String name){
+     return this.eps1.getContratos();
         }
-        return m;
+       
+    
+    public String verContrarosCodigo(String codigo){
+      return this.eps1.mostrar(codigo);
     }
-    public String verContrarosCodigo(String name,String codigo){
-        EPS eps = eps1.get(name);
-        String m=null;
-        if(eps!=null){
-            m=eps.mostrar(codigo);
-        }
-        return m;
+     public String verContraros(){
+      return this.eps1.mostrarDatos();
     }
     
+    public ArrayList<Medico> Traer(String Fecha, LocalTime hora){
+        ArrayList<Medico> disponibles = new ArrayList<>();
+        for (String keyMedico : keyMedicos) {
+            Medico med = (Medico) this.usuarios.get(keyMedico);
+            if(med.estoyDisponible(Fecha, hora)){
+                disponibles.add(med);
+            }
+        }
+       return disponibles;
+    }
+    public String AgregarCita(Medico medi, String Fecha, LocalTime  hora, String Paciente ){
+        
+        
+        medi.addCita(Fecha, hora, Paciente);
+        this.usuarios.replace(medi.getLogin(), medi);
+        return "Cita Agregada";
+    }
+    
+    public String AgregarAgenda(Medico medi, String fecha, String horaInicioM, String  horaFinM, String horaInicioT, String horaFinT){
+        medi.addDisponibilidad(fecha, LocalTime.parse(horaInicioM), LocalTime.parse(horaFinM),LocalTime.parse(horaInicioT), LocalTime.parse(horaFinT));
+        this.usuarios.replace(medi.getLogin(), medi);
+        return "Disponibilidad Añadida";
+    }
+            
+            
     public HashMap<String, Usuario> getUsuarios() {
         return usuarios;
     }
 
-    public HashMap<String, EPS> getEps1() {
+    public EPS getEps1() {
         return eps1;
     }
+    public void Addcontrato(Componente c){
+    eps1.addCont(c);
+    
+    }
+    
+    
+    
 }
