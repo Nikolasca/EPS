60fbdec0b2306c4840c46e515bfdccd53393c23c
julian <juliohmhaha@hotmail.com>
2019-10-11 04:32:11 -0500

    astro

--- src/main/java/Facade/Facede.java
+++ src/main/java/Facade/Facede.java
@@ -39,7 +39,7 @@ public class Facede {
         this.addPaciente("nikolasp", "123","34567","Nikolas","21","armenia","23456789");
         this.addAdministrador("nikolasa", "123","34567","Nikolas","21","armenia","23456789");
         Medico med = (Medico) this.usuarios.get("nikolas");
-        this.AgregarAgenda(med, "12/10/2019","06:00 a. m.", "09:00 a. m.", "06:00 a. m.", "09:00 a. m.");
+        this.AgregarAgenda(med, "12/10/2019","06:00", "09:00", "06:00", "09:00");
         
     }
     public Usuario addMedico(String login,String password,String Cedula,String Nombres,String Fecha,String Direccion, String Telefono) {
