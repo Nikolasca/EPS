f32513b30aedbaaee50e9c42a191c4ae9dfabf18
julian <juliohmhaha@hotmail.com>
2019-10-11 04:50:39 -0500

    astro

---
+++ src/main/java/launch/main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package launch;
+
+import Adapter.Medico;
+import Facade.Facede;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Julio
+ */
+public class main {
+
+    /*public static void main(String[] args) {
+        Facede fac = Facede.rConstructora();
+        Medico medi = (Medico) fac.getUsuarios().get("nikolas");
+        System.out.println(medi.estoyDisponible("12/10/2019",LocalTime.parse("07:00")));
+        ArrayList<Medico> med = fac.Traer("12/10/2019", "07:00");
+        System.out.println(med.size());
+        
+    }
+    */
+}
